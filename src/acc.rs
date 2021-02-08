4e6d0f3bb4b54891c2841b981b0155ae4c6f6e1f
Cheng XU <git@xuc.me>
2021-02-07 21:36:23 -0800

    acc: add type declarations

--- src/acc.rs
+++ src/acc.rs
@@ -5,3 +5,10 @@ pub mod keys;
 pub mod serde_impl;
 pub mod set;
 pub mod utils;
+
+use ark_bls12_381::Bls12_381;
+pub type AccSecretKey = keys::AccSecretKey<Bls12_381>;
+pub type AccPublicKey = keys::AccPublicKey<Bls12_381>;
+pub type LeftAccValue = acc_values::LeftAccValue<Bls12_381>;
+pub type RightAccValue = acc_values::RightAccValue<Bls12_381>;
+pub type AccValue = acc_values::AccValue<Bls12_381>;
