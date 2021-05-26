a92eb07232d644b942ee356ba8e4d9132ea18910
Haixin Wang <jasonxunhkbu@gmail.com>
2021-05-26 11:27:34 +0800

    move unused comments

--- src/chain/hash.rs
+++ src/chain/hash.rs
@@ -1,6 +1,5 @@
-#![allow(unused)]
-use super::{block::BlockId, /*id_tree::IdTreeObjId, */ object::ObjId, range::Range, traits::Num,};
-use crate::digest::{blake2, concat_digest, /*concat_digest_ref,*/ Digest, Digestible};
+use super::{block::BlockId, object::ObjId, range::Range, traits::Num};
+use crate::digest::{blake2, concat_digest, Digest, Digestible};
 use std::collections::HashSet;
 
 #[inline]
@@ -8,38 +7,6 @@ pub(crate) fn range_hash<K: Num>(range: &Range<K>) -> Digest {
     let mut state = blake2().to_state();
     state.update(range.get_low().to_digest().as_bytes());
     state.update(range.get_high().to_digest().as_bytes());
-    Digest::from(state.finalize())
-}
-
-// #[inline]
-// pub(crate) fn trie_leaf_hash(acc_hash: Digest) -> Digest {
-//     acc_hash
-// }
-
-// #[inline]
-// pub(crate) fn trie_non_leaf_hash<'a>(
-//     nibble: &str,
-//     acc_hash: &Digest,
-//     child_hashes: impl Iterator<Item = &'a Digest>,
-// ) -> Digest {
-//     let mut state = blake2().to_state();
-//     state.update(nibble.to_digest().as_bytes());
-//     state.update(acc_hash.as_bytes());
-//     state.update(concat_digest_ref(child_hashes).as_bytes());
-//     Digest::from(state.finalize())
-// }
-
-#[inline]
-pub(crate) fn block_head_hash<'a>(
-    block_id: BlockId,
-    prev_hash: &Digest,
-    id_tree_root_hash: &Digest,
-    ads_hash: &Digest,
-) -> Digest {
-    let mut state = blake2().to_state();
-    state.update(&block_id.to_le_bytes());
-    state.update(prev_hash.as_bytes());
-    state.update(&id_tree_root_hash.as_bytes());
     Digest::from(state.finalize())
 }
 
