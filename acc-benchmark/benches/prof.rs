7a941d1ecf43a1c72dedd033406eaa8f395daeb4
Cheng XU <git@xuc.me>
2021-08-22 01:17:52 -0700

    fmt

--- acc-benchmark/benches/prof.rs
+++ acc-benchmark/benches/prof.rs
@@ -1,5 +1,5 @@
 use acc_benchmark::*;
-use criterion::{criterion_group, criterion_main, Criterion, SamplingMode};
+use criterion::{criterion_group, criterion_main, Criterion};
 use pprof::criterion::{Output, PProfProfiler};
 
 const Q: u64 = 500;
