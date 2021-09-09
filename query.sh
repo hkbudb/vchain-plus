d5db35840e34f7d7ed60d86d115c4b698fe1af51
Haixin Wang <jasonxunhkbu@gmail.com>
2021-09-09 11:34:21 +0800

    update query script

--- query.sh
+++ query.sh
@@ -11,7 +11,11 @@ for db in ${DBS[*]}
 do
     for q in ${QS[*]}
     do
-        printf "query: %s, db: %s\n" ${q} ${db}
-        cargo run --release --bin query -- -k ./keys/254_4096 -i data/db/${DB_DIR}/${db} -q ./data/query/generated_query/${q}/vchain_plus.json -r ./data/result/${DB_DIR}/${db}/${q}_query_res.json
+        printf "query: %s, db: %s\n using basic approach" ${q} ${db}
+        cargo run --release --bin query -- -o 0 -k ./keys/254_4096 -i data/db/${DB_DIR}/${db} -q ./data/query/generated_query/${q}/vchain_plus.json -r ./data/result/${DB_DIR}/${db}/${q}_basic_query.json
+        printf "query: %s, db: %s\n using trimmed approach" ${q} ${db}
+        cargo run --release --bin query -- -o 2 -k ./keys/254_4096 -i data/db/${DB_DIR}/${db} -q ./data/query/generated_query/${q}/vchain_plus.json -r ./data/result/${DB_DIR}/${db}/${q}_trimmed_query.json
+        printf "query: %s, db: %s\n using trimmed2 approach" ${q} ${db}
+        cargo run --release --bin query -- -o 1 -k ./keys/254_4096 -i data/db/${DB_DIR}/${db} -q ./data/query/generated_query/${q}/vchain_plus.json -r ./data/result/${DB_DIR}/${db}/${q}_trimmed2.json
     done
 done
