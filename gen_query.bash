7fbd4e44183add0036a1acc96f38168579c1b186
Haixin Wang <jasonxunhkbu@gmail.com>
2021-09-09 15:42:43 +0800

    update gen_query

--- gen_query.bash
+++ gen_query.bash
@@ -2,7 +2,7 @@
 mkdir -p ./data/query/generated_query
 
 printf "generating range queries"
-cargo run --release --bin gen_query -- -r -t 10 -d 1 -s 0.0001 -g 500 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t10_s0001
+#cargo run --release --bin gen_query -- -r -t 10 -d 1 -s 0.0001 -g 500 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t10_s0001
 cargo run --release --bin gen_query -- -r -t 50 -d 1 -s 0.001 -g 1000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t50_s001
 cargo run --release --bin gen_query -- -r -t 100 -d 1 -s 0.01 -g 1000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t100_s01
 cargo run --release --bin gen_query -- -r -t 500 -d 1 -s 0.05 -g 2000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t500_s05
@@ -26,9 +26,9 @@ cargo run --release --bin gen_query -- -k -f -t 1000 -n 2 -s 0.1 -i ./data/db/et
 cargo run --release --bin gen_query -- -k -f -t 1000 -n 4 -s 0.1 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/keyword_t1000_n4
 
 printf "generating keyword range queries"
-cargo run --release --bin gen_query -- -b -f -t 10 -d 1 -s 0.0001 -n 1 -g 500 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/both_t10_n1
-cargo run --release --bin gen_query -- -b -f -t 10 -d 1 -s 0.0001 -n 2 -g 500 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/both_t10_n2
-cargo run --release --bin gen_query -- -b -f -t 10 -d 1 -s 0.0001 -n 4 -g 500 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/both_t10_n4
+#cargo run --release --bin gen_query -- -b -f -t 10 -d 1 -s 0.0001 -n 1 -g 500 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/both_t10_n1
+#cargo run --release --bin gen_query -- -b -f -t 10 -d 1 -s 0.0001 -n 2 -g 500 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/both_t10_n2
+#cargo run --release --bin gen_query -- -b -f -t 10 -d 1 -s 0.0001 -n 4 -g 500 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/both_t10_n4
 cargo run --release --bin gen_query -- -b -f -t 50 -d 1 -s 0.001 -n 1 -g 1000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/both_t50_n1
 cargo run --release --bin gen_query -- -b -f -t 50 -d 1 -s 0.001 -n 2 -g 1000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/both_t50_n2
 cargo run --release --bin gen_query -- -b -f -t 50 -d 1 -s 0.001 -n 4 -g 1000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/both_t50_n4
