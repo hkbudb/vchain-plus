44f2196bb8c22d62513817ceb8fec07a48f42bcb
Haixin Wang <jasonxunhkbu@gmail.com>
2021-09-29 15:26:54 +0800

    update query generation script

--- gen_query.bash
+++ gen_query.bash
@@ -1,12 +1,12 @@
 #!/bin.bash
 mkdir -p ./data/query/generated_query
 
-printf "generating range queries"
-#cargo run --release --bin gen_query -- -r -t 10 -d 1 -s 0.0001 -g 500 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t10_s0001
-cargo run --release --bin gen_query -- -r -t 50 -d 1 -s 0.001 -g 1000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t50_s001
-cargo run --release --bin gen_query -- -r -t 100 -d 1 -s 0.01 -g 1000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t100_s01
-cargo run --release --bin gen_query -- -r -t 500 -d 1 -s 0.05 -g 2000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t500_s05
-cargo run --release --bin gen_query -- -r -t 1000 -d 1 -s 0.1 -g 4000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t1000_s1
+# printf "generating range queries"
+# cargo run --release --bin gen_query -- -r -t 10 -d 1 -s 0.0001 -g 500 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t10_s0001
+# cargo run --release --bin gen_query -- -r -t 50 -d 1 -s 0.001 -g 1000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t50_s001
+# cargo run --release --bin gen_query -- -r -t 100 -d 1 -s 0.01 -g 1000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t100_s01
+# cargo run --release --bin gen_query -- -r -t 500 -d 1 -s 0.05 -g 2000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t500_s05
+# cargo run --release --bin gen_query -- -r -t 1000 -d 1 -s 0.1 -g 4000 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/range_t1000_s1
 
 printf "generating keyword queries"
 cargo run --release --bin gen_query -- -k -f -t 10 -n 1 -s 0.1 -i ./data/db/eth_small_254/t2_id2_b4 -o ./data/query/generated_query/keyword_t10_n1
