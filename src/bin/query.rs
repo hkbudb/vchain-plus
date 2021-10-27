f5c0cef5315f421b93bc812a15d1a1b8d0e50deb
Haixin Wang <jasonxunhkbu@gmail.com>
2021-10-27 11:05:24 +0800

    disallow egg log

--- src/bin/query.rs
+++ src/bin/query.rs
@@ -47,7 +47,7 @@ struct Opt {
 }
 
 fn main() -> Result<()> {
-    init_tracing_subscriber("info")?;
+    init_tracing_subscriber("query=info,vchain_plus::chain=off")?;
     let opts = Opt::from_args();
     let verify_thread_num = opts.verify_thread_num;
     let query_path = opts.query;
